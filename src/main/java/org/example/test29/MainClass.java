ae6504bf2f099e52566abbca9b642055620836eb
PolBadWolf <gm.gameoff@gmail.com>
2020-07-30 10:47:11 +0500

    refactoring

--- src/main/java/org/example/test29/MainClass.java
+++ src/main/java/org/example/test29/MainClass.java
@@ -1,7 +1,9 @@
 package org.example.test29;
 
+import java.io.*;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Properties;
 
 public class MainClass {
 
@@ -9,27 +11,102 @@ public class MainClass {
         new MainClass().start();
     }
 
+    private void defaultProperties(Properties properties, String fileName) {
+        properties.clear();
+        properties.setProperty("URL_Server", "127.0.0.1" );
+        properties.setProperty("Port_Server", "1433");
+        properties.setProperty("DataBase", "spc1");
+        properties.setProperty("User", "max");
+        properties.setProperty("Password", "1122");
+        try {
+            properties.store(new BufferedWriter(new FileWriter(fileName)), "access base");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
     private void start() {
         System.out.println("start programm");
 
+        Properties properties = new Properties();
+        String connectionString;
+        Connection connection = null;
+
+        // загрузка
         try {
-            String instanceName = "127.0.0.1\\SQLEX:1433";
-            String databaseName = "spc1";
-            String userName = "max";
-            String pass = "1122";
-            String connectionUrl = "jdbc:sqlserver://%1$s;databaseName=%2$s;user=%3$s;password=%4$s;";
-            String connectionString = String.format(connectionUrl, instanceName, databaseName, userName, pass);
+            properties.load(new BufferedReader(new FileReader("z1.txt")));
+        } catch (IOException e) {
+            // файл отсутствует
+            defaultProperties(properties, "z1.txt");
+        }
 
-            try {
-                Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
+        {
+            String URL_Server = properties.getProperty("URL_Server");
+            String Port_Server = properties.getProperty("Port_Server");
+            String DataBase = properties.getProperty("DataBase");
+            String User = properties.getProperty("User");
+            String Password = properties.getProperty("Password");
+            if ((URL_Server == null)
+                    || (Port_Server == null)
+                    || (DataBase == null)
+                    || (User == null)
+                    || (Password == null)) {
+                defaultProperties(properties, "z1.txt");
+                URL_Server = properties.getProperty("URL_Server");
+                Port_Server = properties.getProperty("Port_Server");
+                DataBase = properties.getProperty("DataBase");
+                User = properties.getProperty("User");
+                Password = properties.getProperty("Password");
             }
+            String connectionUrl = "jdbc:sqlserver://%1$s:%2$s;databaseName=%3$s;user=%4$s;password=%5$s;";
+            connectionString = String.format(connectionUrl
+                    , URL_Server
+                    , Port_Server
+                    , DataBase
+                    , User
+                    , Password);
+        }
 
-            Connection connection = DriverManager.getConnection(connectionString);
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        } catch (ClassNotFoundException e) {
+            //e.printStackTrace();
+            System.out.println("ошибка инициализации драйвера");
+            System.exit(1);
+        }
 
+        try {
+            connection = DriverManager.getConnection(connectionString);
+        } catch (SQLException ex) {
+            //ex.printStackTrace();
+            System.out.println("ошибка подключения к серверу");
+            System.exit(1);
+        }
+
+        try {
+            PreparedStatement statement;
+
+            ArrayList<Ddt> tt = new ArrayList<>();
+            for (int i = 0; i < 721_001; i++) {
+                tt.add(new Ddt(i, i));
+            }
+            byte[] q = Ddt.toBytes( tt.toArray(new Ddt[tt.size()]));
+            Blob blob = new MyBlob(q);
+
+            statement = connection.prepareStatement("INSERT INTO Table_1(id_spec, ves, dis) VALUES (?, ?, ?)");
+            statement.setInt(1, 66);
+            statement.setInt(2, 77);
+            statement.setBlob(3, blob);
+
+            statement.executeUpdate();
+            statement.close();
+
+        } catch (java.lang.Throwable e) {
+            e.printStackTrace();
+        }
+
+        try {
             Statement stmt = connection.createStatement();
-
             ResultSet executeQuery = stmt.executeQuery("SELECT * FROM spc1.dbo.Table_1");
 
             while (executeQuery.next()) {
@@ -39,36 +116,16 @@ public class MainClass {
 
                 System.out.print(id_spec + "\t" + ves);
                 if (blob != null) {
-                    System.out.print("\tdis len = " + blob.length());
+                    System.out.print("\tdis len = " + blob.length() / 6);
                 }
                 System.out.println();
             }
 
             executeQuery.close();
             stmt.close();
-
-            PreparedStatement statement = connection.prepareStatement("INSERT INTO Table_1(id_spec, ves, dis) VALUES (?, ?, ?)");
-            statement.setInt(1, 66);
-            statement.setInt(2, 77);
-
-            ArrayList<Ddt> tt = new ArrayList<>();
-            for (int i = 1; i < 200_001; i++) {
-                tt.add(new Ddt(i, i));
-            }
-
-            byte[] q = Ddt.toBytes( tt.toArray(new Ddt[tt.size()]));
-
-            Blob blob = new javax.sql.rowset.serial.SerialBlob(q);
-
-            statement.setBlob(3, blob);
-
-            statement.executeUpdate();
-            statement.close();
-
-
-        } catch (SQLException e) {
+        } catch (java.lang.Throwable e) {
             e.printStackTrace();
         }
-    }
 
+    }
 }
